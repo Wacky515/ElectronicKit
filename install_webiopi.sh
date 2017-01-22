9cc1ae95f017270e0effc451aa865544cf4d6715
wacky515 <wacky515@gmail.com>
2017-01-22 14:59:44 +0900

    Add download site

--- install_webiopi.sh
+++ install_webiopi.sh
@@ -6,7 +6,9 @@ source ~/dotfiles/function/result_echo.sh
 readonly PROCESS="install WebIOPi"
 
 ym_echo ">> ${PROCESS^}"
-wget http://webiopi.googlecode.com/files/WebIOPi-0.6.0.tar.gz
+# wget http://webiopi.googlecode.com/files/WebIOPi-0.6.0.tar.gz
+wget http://sourceforge.net/projects/webiopi/files/ \
+    WebIOPi-0.7.1.tar.gz/download
 tar xvzf WebIOPi-0.6.0.tar.gz
 cd WebIOPi-0.6.0
 sudo ./setup.sh
