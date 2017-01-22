51b38b40687d9e4308100e95d299988322cc6220
wacky515 <wacky515@gmail.com>
2017-01-22 15:46:17 +0900

    Revise command 3

--- install_webiopi.sh
+++ install_webiopi.sh
@@ -11,15 +11,21 @@ readonly VER="0.7.1"
 
 ym_echo ">> ${PROCESS^}"
 # wget http://sourceforge.net/projects/webiopi/files/ \
-#     WebIOPi-$VER.tar.gz/download
-# wget "http://sourceforge.net/projects/webiopi/files/
-#     WebIOPi-$VER.tar.gz"
-wget "http://sourceforge.net/projects/webiopi/files/WebIOPi-0.7.1.tar.gz"
+#     WebIOPi-${VER}.tar.gz/download
+# wget http://sourceforge.net/projects/webiopi/files/WebIOPi-"${VER}".tar.gz
+wget http://sourceforge.net/projects/webiopi/files/WebIOPi-${VER}.tar.gz
+# wget "http://sourceforge.net/projects/webiopi/files/WebIOPi-0.7.1.tar.gz"
 result_echo $? "download WebIOPi"
-# tar xvzf "WebIOPi-$VER.tar.gz"
+# tar xvzf WebIOPi-"${VER}".tar.gz
 tar xvzf "WebIOPi-0.7.1.tar.gz"
 result_echo $? "unzip WebIOPi"
-# cd WebIOPi-$VER
+# cd WebIOPi-"${VER}"
 cd WebIOPi-0.7.1
+result_echo $? "download patch"
+wget https://raw.githubusercontent.com/doublebind/raspi/master/ \
+    webiopi-pi2bplus.patch
+patch -p1 -i webiopi-pi2bplus.patch
+result_echo $? "download patch"
+
 sudo ./setup.sh
 result_echo $? ${PROCESS}
