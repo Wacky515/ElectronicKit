15655819bee8cf1980abcbce3c6f4a4a7742d6a7
wacky515 <wacky515@gmail.com>
2017-01-23 21:09:22 +0900

    Add cm_locale_error.sh

--- auto_startup_webiopi.sh
+++ auto_startup_webiopi.sh
@@ -7,8 +7,7 @@ readonly PROCESS="set auto start up WebIOPi"
 
 ym_echo ">> ${PROCESS^}"
 # sudo service webiopi start
-# sudo systemctl enable webiopi
 sudo systemctl daemon-reload && \
-sudo systemctl start webiopi.service && \
+sudo systemctl enable webiopi.service && \
 systemctl status webiopi.service && \
 result_echo $? ${PROCESS}
