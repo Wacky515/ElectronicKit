288144fe977854bbe68814c98518d40ce6abd11d
wacky515 <wacky515@gmail.com>
2017-01-25 20:37:25 +0900

    Add copy cmd

--- test_init_webiopi.sh
+++ test_init_webiopi.sh
@@ -5,3 +5,4 @@ sudo systemctl webiopi stop
 sudo systemctl webiopi start
 sudo chown -R pi /usr/share/webiopi/htdocs
 cp -r ./motor /usr/share/webiopi/htdocs
+cp -r ./motor_twin /usr/share/webiopi/htdocs
